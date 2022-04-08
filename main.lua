ef83c29e92ad5cb716d3c1fca0cdfd63ecb33524
Henrique Farias <91035085+PhariasHref@users.noreply.github.com>
2022-04-08 12:38:20 -0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,21 @@
+require("src/player")
+require("src/textures")
+require("maps/main_map")
+
+function eval(x)
+  return loadstring("return "..x)()
+end
+
+function love.load()
+  textures.load()
+  player.load()
+  main_map.load()
+end
+
+function love.update(dt)
+  main_map.update()
+end
+
+function love.draw()
+  main_map.draw()
+end
