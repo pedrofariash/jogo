ae5140233539c3abf22f5c775948ac7c6fe292c6
Henrique Farias <91035085+PhariasHref@users.noreply.github.com>
2022-04-08 12:40:30 -0300

    Add files via upload

--- src/player.lua
+++ src/player.lua
@@ -1,1 +1,45 @@
+player = {
+  x = 200, y = 300-70,
+  dirsH = {"left", "right"}, dirsV = {"up", "down"},
+  dir = {}
+}
 
+function player.load()
+  player.sprite = textures.resources[1]
+  player.rec = love.graphics.newQuad(0, 0, 40, 80, player.sprite:getWidth(), player.sprite:getHeight())
+end
+
+function player.movementH()
+  for a = 1, 2 do
+    if love.keyboard.isDown(player.dirsH[a]) then
+      eval("player.dir." .. player.dirsH[a] .. "()")
+    end
+  end
+end
+function player.movementV()
+  for a = 1, 2 do
+    if love.keyboard.isDown(player.dirsV[a]) then
+      eval("player.dir." .. player.dirsV[a] .. "()")
+    end
+  end
+end
+
+function player.draw()
+  love.graphics.draw(player.sprite, player.rec, player.x, player.y)
+end
+
+function player.dir.left()
+  player.x = player.x - 5
+end
+
+function player.dir.up()
+  player.y = player.y - 5
+end
+
+function player.dir.right()
+  player.x = player.x + 5
+end
+
+function player.dir.down()
+  player.y = player.y + 5
+end
--- src/textures.lua
+++ src/textures.lua
@@ -1,1 +1,16 @@
+textures = {
+	paths = {
+		"ocon",
+		"planet1",
+		"planet2",
+		"planet3",
+		"way"
+	},
+	resources = {}
+}
 
+function textures.load()
+	for a = 1, 5 do
+		textures.resources[a] = love.graphics.newImage("assets/" .. textures.paths[a] .. ".png")
+	end
+end
